443bcc74643da94bf71792df6bbfbb3a58fc3030
Natania Allan <44893504+natallan07@users.noreply.github.com>
2022-01-06 21:09:31 -0500

    Create gestures.js

---
+++ Grove-kit-sensor/gestures.js
@@ -0,0 +1,27 @@
+grove.onGesture(GroveGesture.Down, function () {
+    basic.showString("D")
+})
+grove.onGesture(GroveGesture.Wave, function () {
+    basic.showString("W")
+})
+grove.onGesture(GroveGesture.Right, function () {
+    basic.showString("R")
+})
+grove.onGesture(GroveGesture.Backward, function () {
+    basic.showString("B")
+})
+grove.onGesture(GroveGesture.Clockwise, function () {
+    basic.showString("C")
+})
+grove.onGesture(GroveGesture.Up, function () {
+    basic.showString("U")
+})
+grove.onGesture(GroveGesture.Forward, function () {
+    basic.showString("F")
+})
+grove.onGesture(GroveGesture.Left, function () {
+    basic.showString("L")
+})
+grove.onGesture(GroveGesture.Anticlockwise, function () {
+    basic.showString("A")
+})

7004d29e0252fea62bb5fe63396933c3e6ffa633
Natania Allan <44893504+natallan07@users.noreply.github.com>
2022-01-06 21:10:40 -0500

    Create gesture-clockwise-anticlockwise.js

---
+++ Grove-kit-sensor/gesture-clockwise-anticlockwise.js
@@ -0,0 +1,18 @@
+grove.onGesture(GroveGesture.Clockwise, function () {
+    basic.showLeds(`
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        `)
+})
+grove.onGesture(GroveGesture.Anticlockwise, function () {
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+})
